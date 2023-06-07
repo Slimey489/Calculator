be0b1bfc6c32e2dabde12b8ff29df7d2efe093a5
Slimey489 <destinyinvective@gmail.com>
2023-06-08 10:06:12 +1200

    Created a basic window

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
+
+    private Dimension mainFrameSize;
+    public JFrame frameMain;
+    public JFrame  mainFrame(){
+        frameMain = new JFrame();
+        mainFrameSize = new Dimension(300,300);
+        frameMain.setPreferredSize(mainFrameSize);
+        frameMain.setVisible(true);
+        return frameMain;
+    }
+
+
+
+
+
+
+
+
+
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new App());
     }
