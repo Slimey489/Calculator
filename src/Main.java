0a365367f0fc917a764ed400079331b9b096be27
Slimey489 <destinyinvective@gmail.com>
2023-06-15 11:28:06 +1200

    Added Operator Buttons & Text Field and the colour scheme has also been changed

--- src/Main.java
+++ src/Main.java
@@ -14,33 +14,88 @@ class Main {
     public ArrayList<String> operation;
     SpringLayout layout;
     public JButton button1,button2,button3,button4,button5,button6,button7,button8,button9,button0;
-    public JButton equalsButton,multiplicationButton,divisionButton,brackets,exponentButton,subtractionButton,additionButton;
+    public JButton equalsButton,multiplicationButton,divisionButton,brackets,exponentButton,subtractionButton,additionButton,decimalButton;
+    public JTextField textField;
+    public JButton clearButton;
     Container objectContainer;
     public JFrame  mainFrame(){
         expression = "";
         frameMain = new JFrame();
-        mainFrameSize = new Dimension(300,300);
+        mainFrameSize = new Dimension(280,305);
         buttonSize = new Dimension(60,30);
+        Dimension textFieldSize = new Dimension(255,80);
+        Color buttonBackgroundColour = Color.GRAY;
+        Color buttonForegroundColour = new Color(243, 242, 224);
+
         objectContainer = frameMain.getContentPane();
         layout = new SpringLayout();
 
+        textField = new JTextField();
         button0 = new JButton("0");
+        button0.setBackground(buttonBackgroundColour);
+        button0.setForeground(buttonForegroundColour);
+
+
         button1 = new JButton("1");
+        button1.setBackground(buttonBackgroundColour);
+        button1.setForeground(buttonForegroundColour);
         button2 = new JButton("2");
+        button2.setBackground(buttonBackgroundColour);
+        button2.setForeground(buttonForegroundColour);
         button3 = new JButton("3");
+        button3.setBackground(buttonBackgroundColour);
+        button3.setForeground(buttonForegroundColour);
         button4 = new JButton("4");
+        button4.setBackground(buttonBackgroundColour);
+        button4.setForeground(buttonForegroundColour);
         button5 = new JButton("5");
+        button5.setBackground(buttonBackgroundColour);
+        button5.setForeground(buttonForegroundColour);
         button6 = new JButton("6");
+        button6.setBackground(buttonBackgroundColour);
+        button6.setForeground(buttonForegroundColour);
         button7 = new JButton("7");
+        button7.setBackground(buttonBackgroundColour);
+        button7.setForeground(buttonForegroundColour);
         button8 = new JButton("8");
+        button8.setBackground(buttonBackgroundColour);
+        button8.setForeground(buttonForegroundColour);
         button9 = new JButton("9");
+        button9.setBackground(buttonBackgroundColour);
+        button9.setForeground(buttonForegroundColour);
         equalsButton = new JButton("=");
+        equalsButton.setBackground(buttonBackgroundColour);
+        equalsButton.setForeground(buttonForegroundColour);
         multiplicationButton = new JButton("x");
+        multiplicationButton.setBackground(buttonBackgroundColour);
+        multiplicationButton.setForeground(buttonForegroundColour);
         divisionButton = new JButton("÷");
+        divisionButton.setBackground(buttonBackgroundColour);
+        divisionButton.setForeground(buttonForegroundColour);
         brackets = new JButton("()");
+        brackets.setBackground(buttonBackgroundColour);
+        brackets.setForeground(buttonForegroundColour);
         exponentButton = new JButton("^x");
+        exponentButton.setBackground(buttonBackgroundColour);
+        exponentButton.setForeground(buttonForegroundColour);
         additionButton = new JButton("+");
+        additionButton.setBackground(buttonBackgroundColour);
+        additionButton.setForeground(buttonForegroundColour);
         subtractionButton = new JButton("-");
+        subtractionButton.setBackground(buttonBackgroundColour);
+        subtractionButton.setForeground(buttonForegroundColour);
+        decimalButton = new JButton(".");
+        decimalButton.setBackground(buttonBackgroundColour);
+        decimalButton.setForeground(buttonForegroundColour);
+        clearButton = new JButton("CE");
+        clearButton.setBackground(buttonBackgroundColour);
+        clearButton.setForeground(buttonForegroundColour);
+
+        textField.setFont(new Font("Calibri",Font.BOLD,50));
+        textField.setForeground(Color.WHITE);
+        textField.setBackground(Color.DARK_GRAY);
+        textField.setBorder(null);
+        objectContainer.setBackground(Color.DARK_GRAY);
 
         button0.addActionListener(new Action());
         button1.addActionListener(new Action());
@@ -59,7 +114,9 @@ class Main {
         exponentButton.addActionListener(new Action());
         additionButton.addActionListener(new Action());
         subtractionButton.addActionListener(new Action());
+        clearButton.addActionListener(new Action());
 
+        textField.setPreferredSize(textFieldSize);
         button0.setPreferredSize(buttonSize);
         button1.setPreferredSize(buttonSize);
         button2.setPreferredSize(buttonSize);
@@ -70,6 +127,18 @@ class Main {
         button7.setPreferredSize(buttonSize);
         button8.setPreferredSize(buttonSize);
         button9.setPreferredSize(buttonSize);
+        decimalButton.setPreferredSize(buttonSize);
+        equalsButton.setPreferredSize(buttonSize);
+        additionButton.setPreferredSize(buttonSize);
+        subtractionButton.setPreferredSize(buttonSize);
+        multiplicationButton.setPreferredSize(buttonSize);
+        divisionButton.setPreferredSize(buttonSize);
+        brackets.setPreferredSize(buttonSize);
+        exponentButton.setPreferredSize(buttonSize);
+        clearButton.setPreferredSize(buttonSize);
+
+        layout.putConstraint(SpringLayout.WEST,textField,5,SpringLayout.WEST,objectContainer);
+        layout.putConstraint(SpringLayout.NORTH,textField,5,SpringLayout.NORTH,objectContainer);
 
         layout.putConstraint(SpringLayout.WEST, button7, 5, SpringLayout.WEST, objectContainer);
 
@@ -84,6 +153,16 @@ class Main {
         layout.putConstraint(SpringLayout.WEST, button6, 5, SpringLayout.EAST, button5);
         layout.putConstraint(SpringLayout.WEST, button8, 5, SpringLayout.EAST, button7);
         layout.putConstraint(SpringLayout.WEST, button9, 5, SpringLayout.EAST, button8);
+        layout.putConstraint(SpringLayout.WEST,decimalButton,0,SpringLayout.WEST,button3);
+        layout.putConstraint(SpringLayout.WEST,equalsButton,5,SpringLayout.EAST,decimalButton);
+        layout.putConstraint(SpringLayout.WEST,additionButton,0,SpringLayout.WEST,equalsButton);
+        layout.putConstraint(SpringLayout.WEST,subtractionButton,0,SpringLayout.WEST,additionButton);
+        layout.putConstraint(SpringLayout.WEST,multiplicationButton,0,SpringLayout.WEST,subtractionButton);
+        layout.putConstraint(SpringLayout.WEST,divisionButton,0,SpringLayout.WEST,multiplicationButton);
+        layout.putConstraint(SpringLayout.WEST,brackets,0,SpringLayout.WEST,button7);
+        layout.putConstraint(SpringLayout.WEST,exponentButton,0,SpringLayout.WEST,button8);
+        layout.putConstraint(SpringLayout.WEST,clearButton,0,SpringLayout.WEST,button9);
+
 
         layout.putConstraint(SpringLayout.NORTH, button0, 5, SpringLayout.SOUTH, button2);
         layout.putConstraint(SpringLayout.NORTH, button1, 5, SpringLayout.SOUTH, button4);
@@ -94,7 +173,17 @@ class Main {
         layout.putConstraint(SpringLayout.NORTH, button6, 5, SpringLayout.SOUTH, button9);
         layout.putConstraint(SpringLayout.NORTH, button8, 0, SpringLayout.NORTH,button7);
         layout.putConstraint(SpringLayout.NORTH, button9, 0, SpringLayout.NORTH, button7);
+        layout.putConstraint(SpringLayout.NORTH,decimalButton,0,SpringLayout.NORTH,button0);
+        layout.putConstraint(SpringLayout.NORTH,equalsButton,0,SpringLayout.NORTH,button0);
+        layout.putConstraint(SpringLayout.NORTH,additionButton,0,SpringLayout.NORTH,button3);
+        layout.putConstraint(SpringLayout.NORTH,subtractionButton,0,SpringLayout.NORTH,button6);
+        layout.putConstraint(SpringLayout.NORTH,multiplicationButton,0,SpringLayout.NORTH,button9);
+        layout.putConstraint(SpringLayout.SOUTH,divisionButton,-5,SpringLayout.NORTH,multiplicationButton);
+        layout.putConstraint(SpringLayout.SOUTH,brackets,-5,SpringLayout.NORTH,button7);
+        layout.putConstraint(SpringLayout.SOUTH,exponentButton,-5,SpringLayout.NORTH,button8);
+        layout.putConstraint(SpringLayout.SOUTH,clearButton,-5,SpringLayout.NORTH,button9);
 
+        objectContainer.add(textField);
         objectContainer.add(button0);
         objectContainer.add(button1);
         objectContainer.add(button2);
@@ -105,6 +194,15 @@ class Main {
         objectContainer.add(button7);
         objectContainer.add(button8);
         objectContainer.add(button9);
+        objectContainer.add(equalsButton);
+        objectContainer.add(additionButton);
+        objectContainer.add(subtractionButton);
+        objectContainer.add(multiplicationButton);
+        objectContainer.add(divisionButton);
+        objectContainer.add(brackets);
+        objectContainer.add(exponentButton);
+        objectContainer.add(decimalButton);
+        objectContainer.add(clearButton);
 
 
         objectContainer.setSize(mainFrameSize);
@@ -114,56 +212,66 @@ class Main {
         return frameMain;
     }
     private class Action implements ActionListener{
-        static String eSource;
         public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == button0){
-                expression = expression + "0";
+            if (e.getSource() == button0) {
+                expression = textField.getText() + 0;
+                textField.setText(expression);
+            } else if (e.getSource() == button1) {
+                expression = textField.getText() + 1;
+                textField.setText(expression);
+            } else if (e.getSource() == button2) {
+                expression = textField.getText() + 2;
+                textField.setText(expression);
+            } else if (e.getSource() == button3) {
+                expression = textField.getText() + 3;
+                textField.setText(expression);
+            } else if (e.getSource() == button4) {
+                expression = textField.getText() + 4;
+                textField.setText(expression);
+            } else if (e.getSource() == button5) {
+                expression = textField.getText() + 5;
+                textField.setText(expression);
+            } else if (e.getSource() == button6) {
+                expression = textField.getText() + 6;
+                textField.setText(expression);
+            } else if (e.getSource() == button7) {
+                expression = textField.getText() + 7;
+                textField.setText(expression);
+            } else if (e.getSource() == button8) {
+                expression = textField.getText() + 8;
+                textField.setText(expression);
+            } else if (e.getSource() == button9) {
+                expression = textField.getText() + 9;
+                textField.setText(expression);
+            } else if (e.getSource() == brackets) {
+                expression = textField.getText() + "()";
+                textField.setText(expression);
+            } else if (e.getSource() == multiplicationButton) {
+                expression = textField.getText() + "*";
+                textField.setText(expression);
+            } else if (e.getSource() == divisionButton) {
+                expression = textField.getText() + "/";
+                textField.setText(expression);
+            } else if (e.getSource() == additionButton) {
+                expression = textField.getText() + "+";
+                textField.setText(expression);
+            } else if (e.getSource() == subtractionButton) {
+                expression = textField.getText() + "-";
+                textField.setText(expression);
+            } else if (e.getSource() == exponentButton) {
+                expression = textField.getText() + "^";
+                textField.setText(expression);
+            } else if (e.getSource() == equalsButton){
+                expression = textField.getText();
+            } else if (e.getSource() == decimalButton) {
+                expression = textField.getText()+".";
+                textField.setText(expression);
+            } else if (e.getSource() == clearButton) {
+                textField.setText("");
+                expression = textField.getText();
+
             }
-            else if (e.getSource() == button1) {
-                expression = expression + "1";
-            }
-            else if (e.getSource() == button2) {
-                expression = expression + "2";
-            }
-            else if (e.getSource() == button3) {
-                expression = expression + "3";
-            }
-            else if (e.getSource() == button4) {
-                expression = expression + "4";
-            }
-            else if (e.getSource() == button5) {
-                expression = expression + "5";
-            }
-            else if (e.getSource() == button6) {
-                expression = expression + "6";
-            }
-            else if (e.getSource() == button7) {
-                expression = expression + "7";
-            }
-            else if (e.getSource() == button8) {
-                expression = expression + "8";
-            }
-            else if (e.getSource() == button9) {
-                expression = expression + "9";
-            }
-            else if (e.getSource() == brackets) {
-                expression = expression + "()";
-            }
-            else if (e.getSource() == multiplicationButton) {
-                expression = expression + "*";
-            }
-            else if (e.getSource() == divisionButton) {
-                expression = expression + "/";
-            }
-            else if (e.getSource() == additionButton) {
-                expression = expression + "+";
-            }
-            else if (e.getSource() == subtractionButton) {
-                expression = expression + "-";
-            }
-            else if (e.getSource() == exponentButton) {
-                expression = expression + "^";
-            }
+
             System.out.println(expression);
 
         }
