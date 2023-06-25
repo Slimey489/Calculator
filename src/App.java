364c4c0b7bf23a0d73c0d9213cceaec831ae4c52
Slimey489 <destinyinvective@gmail.com>
2023-06-25 16:56:26 +1200

    simplified App class

--- src/App.java
+++ src/App.java
@@ -1,10 +1,6 @@
-import javax.swing.*;
-
 public class App implements Runnable {
-    private JFrame gui;
     public void run(){
-            Main classInstance = new Main();
-            gui = classInstance.mainFrame();
-
+             Menu classInstance = new Menu();
+             classInstance.menuFrame();
     }
 }
